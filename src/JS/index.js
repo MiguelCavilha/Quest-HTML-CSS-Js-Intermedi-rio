9a513cca4697aadbcfab983ae3e1f794b6464d48
Luiz Miguel Cavilha <miguelcavilha@outlook.com>
2023-02-09 19:27:55 -0300

    trabalho em andamento

---
+++ src/JS/index.js
@@ -0,0 +1,14 @@
+const areaInputs = document.querySelectorAll ("input");
+console.log(areaInputs)
+
+const areaTexto = document.querySelector ("textarea");
+console.log(areaTexto)
+
+const confirmarBotao = document.querySelector ("button");
+console.log(confirmarBotao)
+
+
+
+confirmarBotao.addEventListener("click", function (){
+       
+})
