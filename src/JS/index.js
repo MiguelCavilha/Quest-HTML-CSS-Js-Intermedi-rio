47af3a5b148dbd0cbb5eca1a78e24a860aaa7099
Luiz Miguel Cavilha <miguelcavilha@outlook.com>
2023-03-11 09:42:39 -0300

    Formulário completo

--- src/JS/index.js
+++ src/JS/index.js
@@ -8,23 +8,39 @@ confirmarBotao.addEventListener("click", () =>{
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
 
+        
 
     })
 
     inputsFormulario.forEach((input) => {
 
-        const campoOb =
-            input.parentElement.querySelector(".campo-ob");
+        const campoOb = 
+        input.nextElementSibling;
 
         if(!input.value) {
             input.classList.add("invalido");
             campoOb.innerHTML = "campo obrigatório";
-            
+    
         } else {
             input.classList.remove("invalido");
-            campoOb.style.display = "block"
+            campoOb.style.display = "none"
         }
     })
+
+    inputsFormulario.forEach((input) => {
+
+
+        if(!input.value) {
+            input.classList.remove("valido");
+    
+        } else {
+            input.classList.add("valido");
+        }
+    })
+
+    
+
+
 })
 
  
